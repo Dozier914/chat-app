c71cba49109bc2072c246cb0835124e3821837ce
Dozier Atwell <dozier.atwell914@gmail.com>
2020-05-26 16:39:32 -0500

    slight deployment changes

---
+++ client/build/precache-manifest.30e934af928b56199c613fae95e4c0ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32e1a78e84a27b4360bc770aabe7a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2582cec06240fc729852",
+    "url": "/static/css/main.22966569.chunk.css"
+  },
+  {
+    "revision": "1db8fc3f708fa6888d69",
+    "url": "/static/js/2.12305568.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.12305568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2582cec06240fc729852",
+    "url": "/static/js/main.a64a2802.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
